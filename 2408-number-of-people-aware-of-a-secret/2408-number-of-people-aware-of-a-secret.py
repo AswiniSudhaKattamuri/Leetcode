4b5ae65743d54ebfd4176cec9477cd517317f263
Aswini Sudha Kattamuri <aswinisudha49@gmail.com>
2025-09-09 20:11:07 +0530

    Time: 0 ms (100%), Space: 17.8 MB (71.53%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,20 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        
+        MOD = 10**9 + 7
+        dp = [0] * (n + 1)
+        dp[1] = 1  
+        
+        share = 0  
+        
+        for day in range(2, n + 1):
+            
+            if day - delay >= 1:
+                share = (share + dp[day - delay]) % MOD
+       
+            if day - forget >= 1:
+                share = (share - dp[day - forget]) % MOD
+            
+            dp[day] = share  
+        
+        return sum(dp[n - forget + 1 : n + 1]) % MOD
