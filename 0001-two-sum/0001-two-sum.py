a8b3468a41f319b1ac8ddf1d08a5e252692c2595
Aswini Sudha Kattamuri <aswinisudha49@gmail.com>
2025-09-19 16:57:37 +0530

    Time: 0 ms (100%), Space: 18.8 MB (50.8%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic={}
+        for i,num in enumerate(nums):
+            res=target-num
+            if res in dic:
+                return [dic[res],i]
+            dic[num]=i
