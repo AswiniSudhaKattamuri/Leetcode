2ed072cd4c84ec013d27f71517de20b34796512f
Aswini Sudha Kattamuri <aswinisudha49@gmail.com>
2025-12-30 08:35:03 +0530

    Time: 816 ms (58.17%), Space: 44 MB (61.01%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        total=0
+        for i in range(k):
+            value=happiness[i]-i
+            if value>0:
+                total+=value
+            else:
+                break
+        return total
